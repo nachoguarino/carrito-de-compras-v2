2071c0ff24cc1e4fffdf6787f972f7865ba91ca4
“Nacho <nacho.guarino@live.com.ar>
2023-03-22 23:36:12 -0300

    implementando carrito de compras en proceso

--- js/app.js
+++ js/app.js
@@ -8,8 +8,6 @@ class ServicioController {
 
         if (listaJSON) {
             this.listaServicios = JSON.parse(listaJSON)
-        } else {
-            this.listaServicios = []
         }
     }
     filtrarPrecioMax(precio) {
@@ -27,7 +25,7 @@ class ServicioController {
                 <p class="card-text">${servicio.descripcion}</p>
                 <h3 class="card-text">$${servicio.precio}</h3>
                 
-                <a href="#" class="btn btn-primary" id="agregarAlCarrito">Agregar al Carrito</a>
+                <a href="#" class="btn btn-primary" id="servicio${servicio.id}">Agregar al Carrito</a>
             </div>
         </div>`
         })
@@ -37,16 +35,69 @@ class ServicioController {
 
     }
 }
+
+class CarritoController {
+    constructor() {
+        this.listaCarrito = []
+
+    }
+    subir() {
+        let listaJSON = localStorage.getItem("listaCarrito")
+
+        if (listaJSON) {
+            this.listaCarrito = JSON.parse(listaJSON)
+        }
+    }
+    añadirACarrito(servicio) {
+        this.listaCarrito.push(servicio)
+        let arrJSON = JSON.stringify(this.listaCarrito)
+        localStorage.setItem("listaCarrito", arrJSON)
+    }
+
+    mostrarEnDom(nodo) {
+
+        nodo.innerHTML = ""
+
+        this.listaCarrito.forEach(servicio => {
+
+            nodo.innerHTML += `
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="https://yt3.googleusercontent.com/ytc/AL5GRJVyaKyzr5lmzcEMoErT8EGUChYsjfN0iAF-Lnvx=s900-c-k-c0x00ffffff-no-rj" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                        <h5 class="card-title">${servicio.servicio}</h5>
+                        <p class="card-text">${servicio.descripcion}</p>
+                        <h3 class="card-text">$${servicio.precio}</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `
+        })
+    }
+}
+
+
 //DOM
 const servicios_container = document.getElementById("servicios_container")
+const servicios_carrito = document.getElementById("servicios_carrito")
 const precioMax = document.getElementById("filtroPrecio")
 const ordenarPorPrecio = document.getElementById("ordenarPorPrecio")
 
-//APP
+//CONTROLADORES
 const controladorServicios = new ServicioController()
 controladorServicios.subirServicios()
 controladorServicios.ordenarPorPrecio() // ordenar por precio por defecto
 controladorServicios.cargarEnDom(servicios_container)
+
+const controladorCarrito = new CarritoController()
+controladorCarrito.subir()
+
+
+
 
 //FILTRO
 precioMax.addEventListener("change", () => {
@@ -69,4 +120,19 @@ ordenarPorPrecio.addEventListener("click", () => {
     controladorServicios.ordenarPorPrecio()
     controladorServicios.limpiarDom(servicios_container)
     controladorServicios.cargarEnDom(servicios_container)
+})
+
+//AÑADIR AL CARRITO 
+
+controladorServicios.listaServicios.forEach(servicio => {
+
+    const servicioAlCarrito = document.getElementById(`servicio${servicio.id}`)
+
+    servicioAlCarrito.addEventListener("click", () => {
+
+        controladorCarrito.subirServicios(servicio)
+        controladorCarrito.añadirACarrito()
+
+        controladorCarrito.mostrarEnDom(servicios_carrito)
+    })
 })
